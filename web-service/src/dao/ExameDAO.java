7c50fe31b17819faf51b1152961f6f45b64121d3
Daniel Bou <danielbou@outlook.com.br>
2020-07-19 01:23:07 -0300

    WebService SOAP

---
+++ web-service/src/dao/ExameDAO.java
@@ -0,0 +1,107 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import factory.DBUtil;
+import modelo.Exame;
+
+public class ExameDAO {
+	
+	public ExameDAO() {
+		
+	}
+	
+	public static int salvar(Exame exame) {
+
+		int status = 0;
+
+		try {
+			Connection conn = DBUtil.getConnection();
+			PreparedStatement ps= conn.prepareStatement("insert into exame (nomeExame) values (?)");
+			ps.setString(1, exame.getNomeExame());
+			status = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return status;
+	}
+	
+	public static List<Exame> pegarTodosExames() {
+		List<Exame> listaExames = new ArrayList<Exame>();
+		
+		try {
+			Connection conn = DBUtil.getConnection();
+			Statement st= conn.createStatement();
+			ResultSet rs= st.executeQuery("select * from exame");
+			
+			while(rs.next()) {
+				Exame exame = new Exame(rs.getString("nomeExame"));
+				exame.setId(rs.getInt("idExame"));
+				
+				listaExames.add(exame);
+			}
+			DBUtil.closeConnection(conn);
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return listaExames;
+	}
+	
+	public static Exame pegarExamePeloId(String exameId) {
+		Exame exame = null;
+		
+		try {
+			Connection conn = DBUtil.getConnection();
+			PreparedStatement ps= conn.prepareStatement("select * from exame where idExame = ?");
+			ps.setString(1, exameId);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				exame = new Exame(rs.getInt("idExame"), rs.getString("nomeExame"));
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		return exame;
+	}
+	
+	public static int atualizarExame(Exame exame) {
+		int status = 0;
+		try {
+			Connection conn = DBUtil.getConnection();
+			PreparedStatement ps= conn.prepareStatement("update exame set nomeExame=? WHERE idExame=?");
+			ps.setString(1, exame.getNomeExame());
+			ps.setInt(2, exame.getId());
+			status = ps.executeUpdate();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return status;
+	}
+	
+	public static int deletarExame(String exameId) {
+		int status = 0;
+		try {
+			Connection conn = DBUtil.getConnection();
+			PreparedStatement ps= conn.prepareStatement("delete from exame where idExame = ?");
+			ps.setString(1, exameId);
+			status = ps.executeUpdate();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return status;
+	}
+
+}
---
+++ web-service/src/modelo/Exame.java
@@ -0,0 +1,44 @@
+package modelo;
+
+public class Exame {
+	
+	Integer id;
+	String nomeExame;
+	
+	public Exame() {
+		
+	}
+
+	public Exame(String nomeExame) {
+		super();
+		this.nomeExame = nomeExame;
+	}
+	
+	public Exame(Integer id, String nomeExame) {
+		super();
+		this.id = id;
+		this.nomeExame = nomeExame;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNomeExame() {
+		return nomeExame;
+	}
+
+	public void setNomeExame(String nomeExame) {
+		this.nomeExame = nomeExame;
+	}
+
+	@Override
+	public String toString() {
+		return "Exame [id=" + id + ", nomeExame=" + nomeExame + "]";
+	}
+	
+}
